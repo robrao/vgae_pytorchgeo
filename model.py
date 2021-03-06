e7fa442ea5012b0c373fab772b3bbd93c7609bed
Robby Rao <rrao@VSN00313.local>
2021-03-06 13:51:52 -0700

    complete model

--- model.py
+++ model.py
@@ -8,18 +8,31 @@ class VGANet(nn.Module):
         super(VGANet, self).__init__()
 
         # Encoder
-        self.conv1 = GCNConv(input_size, feature_dim)
-        self.conv2 = GCNConv(feature_dim, latent_dim)
+        self.gcn_base = GCNConv(input_size, feature_dim)
+        self.gcn_mean = GCNConv(feature_dim, latent_dim)
+        self.gcn_sig = GCNConv(feature_dim, latent_dim)
 
-        # TODO: Decoder
+    def encode(self, x, edge_index):
+        x = self.gcn_base(x, edge_index)
+        x = F.relu(x)
+        x = F.dropout(x, training=self.training)
+        xu = self.gcn_mean(x, edge_index)
+        xs = self.gcn_sig(x, edge_index)
+
+        gnoise = torch.randn(x.size(0), x.size(1))
+        z_sample = gnoise*torch.exp(xs) + xu
+
+        return z_sample
+
+    def decode(self, z):
+        adj_pred = torch.sigmoid(torch.matmul(z, z.t()))
+
+        return adj_pred
 
     def forward(self, data):
         x, edge_index = data.x, edge_index
 
-        x = self.conv1(x, edge_index)
-        x = F.relu(x)
-        x = F.dropout(x, training=self.training)
-        x = self.conv2(x, edge_index)
+        z = self.encode(x, edge_index)
+        adj_pred = self.decode(z)
 
-        # TODO: implement decoder
-        return x
+        return adj_pred
